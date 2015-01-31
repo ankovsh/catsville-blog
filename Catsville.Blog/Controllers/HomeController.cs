12f9a81fa462264d446e5b94bf7dd879575f61a2
ankovsh <anastasya.ermoshina@gmail.com>
2015-01-31 23:21:15 +0300

    Added project to repository.

---
+++ Catsville.Blog/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace Catsville.Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return RedirectToAction("News");
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        public ActionResult Comments()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
